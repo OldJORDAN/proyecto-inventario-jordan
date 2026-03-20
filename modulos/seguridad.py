75b30adc96d0f38bcac31ea26e9bc9c810b39a06
OldJORDAN <jordantito66@gmail.com>
2026-03-20 13:49:13 -0500

    Update seguridad.py

--- modulos/seguridad.py
+++ modulos/seguridad.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
 def verificar_clave(usuario_ingresado, clave_ingresada, clave_real_excel):
-    """
-    Verifica si los datos coinciden con el Admin Maestro o con el Excel.
-    """
-    # --- LA LLAVE MAESTRA DE JORDAN ---
+    # LLAVE MAESTRA
     ADMIN_MAESTRO = "jordan"
     CLAVE_MAESTRA = "1234"
 
-    # 1. ¿Es el Admin Maestro?
     if usuario_ingresado.lower() == ADMIN_MAESTRO and str(clave_ingresada) == CLAVE_MAESTRA:
         return True, "Desarrollador"
 
-    # 2. Si no es el maestro, comparamos con lo que vino del Excel
-    if str(clave_ingresada).strip() == str(clave_real_excel).strip():
-        return True, None # Devolvemos None para que app.py use el rol del Excel
+    # Comparación normal con Excel
+    ingresada = str(clave_ingresada).strip()
+    real = str(clave_real_excel).strip()
     
-    # Si nada coincide
+    if real.endswith('.0'): real = real[:-2]
+    
+    if ingresada == real:
+        return True, None
+    
     return False, None
